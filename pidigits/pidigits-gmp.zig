cf96c10a03ddd12b14cb602225318190ff0c96fb
Travis Staloch <twostepted@gmail.com>
2020-01-09 21:30:11 -0800

    link gmp functions rather than cIncluding

--- pidigits/pidigits-gmp.zig
+++ pidigits/pidigits-gmp.zig
@@ -7,9 +7,20 @@
 // $ zig build-exe pidigits-gmp.zig -lgmp -lc --library-path /usr/lib/x86_64-linux-gnu/ -isystem /usr/include/x86_64-linux-gnu/
 
 const std = @import("std");
+const c = std.c;
+
+pub extern "c" fn putchar(u8) c_int;
 
 const gmp = @cImport(@cInclude("gmp.h"));
-const c = @cImport(@cInclude("stdio.h"));
+pub extern fn __gmpz_init(gmp.mpz_t) void;
+pub extern fn __gmpz_init_set_ui(gmp.mpz_t, ui) void;
+pub extern fn __gmpz_add(gmp.mpz_t, gmp.mpz_t, gmp.mpz_t) void;
+pub extern fn __gmpz_mul_ui(gmp.mpz_t, gmp.mpz_t, ui) void;
+pub extern fn __gmpz_tdiv_q(gmp.mpz_t, gmp.mpz_t, gmp.mpz_t) void;
+pub extern fn __gmpz_addmul_ui(gmp.mpz_t, gmp.mpz_t, ui) void;
+pub extern fn __gmpz_submul_ui(gmp.mpz_t, gmp.mpz_t, ui) void;
+pub extern fn __gmpz_cmp(gmp.mpz_t, gmp.mpz_t) c_int;
+pub extern fn __gmpz_get_ui(gmp.mpz_t) ui;
 
 const ui = u32;
 var tmp1: gmp.mpz_t = undefined;
@@ -26,26 +37,26 @@ pub fn main() !void {
     const stdout = &std.io.getStdOut().outStream().stream;
     var buf: [10]u8 = undefined;
 
-    gmp.mpz_init(&tmp1);
-    gmp.mpz_init(&tmp2);
+    __gmpz_init(tmp1);
+    __gmpz_init(tmp2);
 
-    gmp.mpz_init_set_ui(&acc, 0);
-    gmp.mpz_init_set_ui(&den, 1);
-    gmp.mpz_init_set_ui(&num, 1);
+    __gmpz_init_set_ui(acc, 0);
+    __gmpz_init_set_ui(den, 1);
+    __gmpz_init_set_ui(num, 1);
 
     var k: u32 = 0;
     var i: u32 = 0;
     while (i < n) {
         k += 1;
         next_term(k);
-        if (gmp.mpz_cmp(&num, &acc) > 0)
+        if (__gmpz_cmp(num, acc) > 0)
             continue;
 
         const d = extract_digit(3);
         if (d != extract_digit(4))
             continue;
 
-        _ = c.putchar('0' + @truncate(u8, d));
+        _ = putchar('0' + @truncate(u8, d));
         i += 1;
 
         if (i % 10 == 0) {
@@ -57,30 +68,23 @@ pub fn main() !void {
 
 fn extract_digit(nth: ui) ui {
     // joggling between tmp1 and tmp2, so GMP won't have to use temp buffers
-    gmp.mpz_mul_ui(&tmp1, &num, nth);
-    gmp.mpz_add(&tmp2, &tmp1, &acc);
-    gmp.mpz_tdiv_q(&tmp1, &tmp2, &den);
-    return mpz_get_ui(&tmp1);
-}
-
-fn mpz_get_ui(__gmp_z: gmp.mpz_srcptr) ui {
-    const __gmp_p = __gmp_z[0]._mp_d;
-    const __gmp_n = __gmp_z[0]._mp_size;
-    const __gmp_l = __gmp_p[0];
-    return @intCast(ui, if (__gmp_n != 0) __gmp_l else 0);
+    __gmpz_mul_ui(tmp1, num, nth);
+    __gmpz_add(tmp2, tmp1, acc);
+    __gmpz_tdiv_q(tmp1, tmp2, den);
+    return __gmpz_get_ui(tmp1);
 }
 
 fn eliminate_digit(d: ui) void {
-    gmp.mpz_submul_ui(&acc, &den, d);
-    gmp.mpz_mul_ui(&acc, &acc, 10);
-    gmp.mpz_mul_ui(&num, &num, 10);
+    __gmpz_submul_ui(acc, den, d);
+    __gmpz_mul_ui(acc, acc, 10);
+    __gmpz_mul_ui(num, num, 10);
 }
 
 fn next_term(k: ui) void {
     const k2 = k * 2 + 1;
 
-    gmp.mpz_addmul_ui(&acc, &num, 2);
-    gmp.mpz_mul_ui(&acc, &acc, k2);
-    gmp.mpz_mul_ui(&den, &den, k2);
-    gmp.mpz_mul_ui(&num, &num, k);
+    __gmpz_addmul_ui(acc, num, 2);
+    __gmpz_mul_ui(acc, acc, k2);
+    __gmpz_mul_ui(den, den, k2);
+    __gmpz_mul_ui(num, num, k);
 }
